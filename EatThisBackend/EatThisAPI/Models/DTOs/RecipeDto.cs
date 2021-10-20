0fe860431faed4d6f22114f847af2b80f511adb9
Łukasz Łopata <lukasz.lopata96@gmail.com>
2021-10-20 22:57:02 +0200

    dailychange

---
+++ EatThisBackend/EatThisAPI/Models/DTOs/RecipeDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EatThisAPI.Models.DTOs
+{
+    public class RecipeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SubName { get; set; }
+        public CategoryDto Category { get; set; }
+        public IngredientQuantity[] IngredientQuantities { get; set; }
+
+    }
+}
