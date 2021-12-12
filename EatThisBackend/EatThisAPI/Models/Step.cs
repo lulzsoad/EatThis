d1a8a39c7f30c025134a06edffbcf747845f5268
Łukasz Łopata <lukasz.lopata96@gmail.com>
2021-12-12 15:24:32 +0100

    Aktualizaja modelu Step

--- EatThisBackend/EatThisAPI/Models/Step.cs
+++ EatThisBackend/EatThisAPI/Models/Step.cs
@@ -9,6 +9,7 @@ namespace EatThisAPI.Models
     {
         public int Id { get; set; }
         public string Description { get; set; }
+        public int Order { get; set; }
 
         public int RecipeId { get; set; }
         public Recipe Recipe { get; set; }
